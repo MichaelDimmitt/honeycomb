30661c4032146c10a89074ef85bd9bc32ac757fb
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-20 11:00:40 -0400

    call drawHive externally, step 1 of breaking everything out into init function

--- index.js
+++ index.js
@@ -15,122 +15,137 @@ const honeyCanvas = (() => {
         }
         grid.push(row)
       }
+      console.log(grid)
       return grid
     })()
-    const drawHive = (hive) => {// This will mirror the hive.
-      const elements = []
-      const root = document.querySelector('#hive')
-      const padding = paddingNum
-      const size = sizeNum
-
-      // IF ADDCLASS DOES NOT GET PASSED AN ELEMENT IT WILL BE A COOL DRAWING PLATFORM.
-      const addClass = ({input: {target: element, className}, dependencies:{indexOf} }) => { // external functions: global:indexOf
-        if (element.className.indexOf(className) === -1) {
-          element.className += (' ' + className)
-        }
+    const elements = []
+    const root = document.querySelector('#hive')
+    const padding = paddingNum
+    const size = sizeNum
+    // IF ADDCLASS DOES NOT GET PASSED AN ELEMENT IT WILL BE A COOL DRAWING PLATFORM.
+    const addClass = ({input: {target: element, className}, dependencies:{indexOf} }) => { // external functions: global:indexOf
+      if (element.className.indexOf(className) === -1) {
+        element.className += (' ' + className)
       }
+    }
 
-      const removeClass = ({ inputs, dependencies }) => {  // external functions: global: split, filter, join
-        element=inputs.target;
-        className=inputs.value;
-        element.className = element.className.split(' ').filter(x => x !== className).join(' ') }
-      const getElementFromCoords = ({input: coords, dependencies}) => { // external functions: non global: elements, done,
-        // console.log(coords, dependencies)
-        const row = dependencies.elements[coords.y]
-        if (row) { const element = row[coords.x]; return element }
-      }
-      const getIntAttr = ({input: {element, attr}}) => { return parseInt(element.getAttribute(attr)) } // external functions: done, global functions parseInt, getAttribute
+    const removeClass = ({ inputs, dependencies }) => {  // external functions: global: split, filter, join
+      element=inputs.target;
+      className=inputs.value;
+      element.className = element.className.split(' ').filter(x => x !== className).join(' ')
+    }
+    const getElementFromCoords = ({input: coords, dependencies}) => { // external functions: non global: elements, done,
+      // console.log(coords, dependencies)
+      const row = dependencies.elements[coords.y]
+      if (row) { const element = row[coords.x]; return element }
+    }
+    const getIntAttr = ({input: {element, attr}}) => { return parseInt(element.getAttribute(attr)) } // external functions: done, global functions parseInt, getAttribute
 
-      const getCoordsFromElement = ({input: element, dependencies: getIntAttr}) => { // external functions: non global: getIntAttr
-        const columnIndex = getIntAttr({input: {element: element, attr: 'data-columnIndex'}});
-        const rowIndex = getIntAttr({input: {element: element, attr: 'data-rowIndex'}})
-        return { x: columnIndex, y: rowIndex } }
+    const getCoordsFromElement = ({input: element, dependencies: getIntAttr}) => { // external functions: non global: getIntAttr
+      const columnIndex = getIntAttr({input: {element: element, attr: 'data-columnIndex'}});
+      const rowIndex = getIntAttr({input: {element: element, attr: 'data-rowIndex'}})
+      return { x: columnIndex, y: rowIndex } }
 
-      const getAdjacentCells = ({input: {coords}, dependencies: {getElementFromCoords}}) => { // external functions: non global: getElementFromCoords, global: filter
-        const x = coords.x; const y = coords.y; const n = y - 1;
-        const s = y + 1; const wx = x - 1; const ex = x + 1;
-        const ny = x % 2 === 1 ? y : n; const sy = x % 2 === 1 ? s : y;
-        return [
-          getElementFromCoords({ input: { x: x, y: n }, dependencies: {elements} }),   // N
-          getElementFromCoords({ input: { x: x, y: s }, dependencies: {elements} }),   // S
-          getElementFromCoords({ input: { x: wx, y: ny }, dependencies: {elements} }), // NW
-          getElementFromCoords({ input: { x: ex, y: ny }, dependencies: {elements} }), // NE
-          getElementFromCoords({ input: { x: wx, y: sy }, dependencies: {elements} }), // SW
-          getElementFromCoords({ input: { x: ex, y: sy }, dependencies: {elements} }), // SE
-        ].filter(x => !!x)
-      }
-      const handleCellMouseOver = (event) => { // external functions: getCoordsFromElement, getAdjacentCells, addClass
-        const target = event.target;
-        const coords = getCoordsFromElement({ input: target, dependencies: getIntAttr })
-        addClass( {input: { target:target, className:'hover'}, dependencies: {indexOf:'test'}}  )
+    const getAdjacentCells = ({input: {coords}, dependencies: {getElementFromCoords}}) => { // external functions: non global: getElementFromCoords, global: filter
+      const x = coords.x; const y = coords.y; const n = y - 1;
+      const s = y + 1; const wx = x - 1; const ex = x + 1;
+      const ny = x % 2 === 1 ? y : n; const sy = x % 2 === 1 ? s : y;
+      return [
+        getElementFromCoords({ input: { x: x, y: n }, dependencies: {elements} }),   // N
+        getElementFromCoords({ input: { x: x, y: s }, dependencies: {elements} }),   // S
+        getElementFromCoords({ input: { x: wx, y: ny }, dependencies: {elements} }), // NW
+        getElementFromCoords({ input: { x: ex, y: ny }, dependencies: {elements} }), // NE
+        getElementFromCoords({ input: { x: wx, y: sy }, dependencies: {elements} }), // SW
+        getElementFromCoords({ input: { x: ex, y: sy }, dependencies: {elements} }), // SE
+      ].filter(x => !!x)
+    }
+    const handleCellMouseOver = (event) => { // external functions: getCoordsFromElement, getAdjacentCells, addClass
+      const target = event.target;
+      const coords = getCoordsFromElement({ input: target, dependencies: getIntAttr })
+      addClass( {input: { target:target, className:'hover'}, dependencies: {indexOf:'test'}}  )
 
-        const adjacentCells = getAdjacentCells({ input: {coords: coords}, dependencies: {getElementFromCoords: getElementFromCoords} })
-        adjacentCells.forEach(cell => {
-          addClass( {input: { target:cell, className:'adjacent'}, dependencies: {indexOf:'test'}} )
-        })
-      }
-      const handleCellMouseOut = (event) => { // external functions: removeClass, getCoordsFromElement, getAdjacentCells
-        const target = event.target
-        const coords = getCoordsFromElement({ input: target, dependencies: getIntAttr })
-
-        removeClass({ inputs: {target: target, value:'hover'}, dependencies: 'test'})
-        const adjacentCells = getAdjacentCells({ input: {coords: coords}, dependencies: {getElementFromCoords: getElementFromCoords} })
-        adjacentCells.forEach(cell => {
-          removeClass({ inputs: {target: cell, value:'adjacent'},dependencies: 'test'})
-        })
-      }
-      const cellFactory = ({inputs, dependencies}) => { // external functions: handleCellMouseOver, handleCellMouseOut
-        // console.log(inputs)
-        let {filled, columnIndex, rowIndex} = inputs
-
-        const div = document.createElement('div')
-        const className = 'cell' + (filled ? ' filled' : '')
-        div.className = className
-        if (filled) {
-          div.addEventListener('mouseover', dependencies.handleCellMouseOver)
-          div.addEventListener('mouseout', dependencies.handleCellMouseOut)
-        }
-        div.setAttribute('data-columnIndex', columnIndex)
-        div.setAttribute('data-rowIndex', rowIndex)
-        return div
-      }
-
-      const penToPaper = (({inputs, dependencies}) => {
-        let {size, padding, root, elements} = inputs;
-        let {hive, cellFactory} = dependencies;
-        hive.forEach((row, rowIndex) => {
-          const elementRow = []
-          row.forEach((value, columnIndex) => {
-            const cell = cellFactory({
-              inputs: { filled:value, columnIndex:columnIndex, rowIndex:rowIndex},
-              dependencies: { handleCellMouseOver, handleCellMouseOut }
-            })
-            const x = (columnIndex * 3 / 4 * size) + padding * columnIndex
-            const y = (rowIndex * size + (columnIndex % 2 === 1 ? (1 / 2 * size) : 0)) + padding * rowIndex
-            cell.style.left = x + 'px'
-            cell.style.top = y + 'px'
-            elementRow.push(cell)
-            root.append(cell)
-          })
-          elements.push(elementRow)
-        })
-      })
-
-      penToPaper({
-        inputs: {size: size, padding: padding, root:root, elements:elements},
-        dependencies: {hive: hive, cellFactory: cellFactory}
+      const adjacentCells = getAdjacentCells({ input: {coords: coords}, dependencies: {getElementFromCoords: getElementFromCoords} })
+      adjacentCells.forEach(cell => {
+        addClass( {input: { target:cell, className:'adjacent'}, dependencies: {indexOf:'test'}} )
       })
     }
+    const handleCellMouseOut = (event) => { // external functions: removeClass, getCoordsFromElement, getAdjacentCells
+      const target = event.target
+      const coords = getCoordsFromElement({ input: target, dependencies: getIntAttr })
+
+      removeClass({ inputs: {target: target, value:'hover'}, dependencies: 'test'})
+      const adjacentCells = getAdjacentCells({ input: {coords: coords}, dependencies: {getElementFromCoords: getElementFromCoords} })
+      adjacentCells.forEach(cell => {
+        removeClass({ inputs: {target: cell, value:'adjacent'},dependencies: 'test'})
+      })
+    }
+    const cellFactory = ({inputs, dependencies}) => { // external functions: handleCellMouseOver, handleCellMouseOut
+      // console.log(inputs)
+      let {filled, columnIndex, rowIndex} = inputs
+
+      const div = document.createElement('div')
+      const className = 'cell' + (filled ? ' filled' : '')
+      div.className = className
+      if (filled) {
+        div.addEventListener('mouseover', dependencies.handleCellMouseOver)
+        div.addEventListener('mouseout', dependencies.handleCellMouseOut)
+      }
+      div.setAttribute('data-columnIndex', columnIndex)
+      div.setAttribute('data-rowIndex', rowIndex)
+      return div
+    }
+
+    const drawHive = (({inputs, dependencies}) => {
+      let {size, padding, root, elements} = inputs;
+      let {hive, cellFactory} = dependencies;
+      hive.forEach((row, rowIndex) => {
+        const elementRow = []
+        row.forEach((value, columnIndex) => {
+          const cell = cellFactory({
+            inputs: { filled:value, columnIndex:columnIndex, rowIndex:rowIndex},
+            dependencies: { handleCellMouseOver, handleCellMouseOut }
+          })
+          const x = (columnIndex * 3 / 4 * size) + padding * columnIndex
+          const y = (rowIndex * size + (columnIndex % 2 === 1 ? (1 / 2 * size) : 0)) + padding * rowIndex
+          cell.style.left = x + 'px'
+          cell.style.top = y + 'px'
+          elementRow.push(cell)
+          root.append(cell)
+        })
+        elements.push(elementRow)
+      })
+    })
+
+
+
     return {
       hive: hive,
-      drawHive: drawHive
+      drawHive: drawHive,
+      elements: elements,
+      root:root,
+      padding:padding,
+      size:size,
+      addClass: addClass,
+      removeClass:removeClass,
+      getElementFromCoords:getElementFromCoords,
+      getIntAttr:getIntAttr,
+      getCoordsFromElement: getCoordsFromElement,
+      getAdjacentCells: getAdjacentCells,
+      handleCellMouseOver: handleCellMouseOver,
+      handleCellMouseOut: handleCellMouseOut,
+      hive: hive,
+      cellFactory: cellFactory
+
     }
     console.log(false)
   }
   function init(preset) {
     let p = playground(preset);
     let hivey = p.hive;
-    p.drawHive(hivey);
+    p.drawHive({
+      inputs: {size: p.size, padding: p.padding, root:p.root, elements:p.elements},
+      dependencies: {hive: p.hive, cellFactory: p.cellFactory}
+    })
   }
 
   return {
